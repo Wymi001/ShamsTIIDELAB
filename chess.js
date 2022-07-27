efa6f39a999957e30c8e3b5bb68d769b2f55a5f0
Clement Adeyemi <adeyemiclement1@gmail.com>
2022-07-27 19:30:14 +0200

    second assignment modified

--- chess.js
+++ chess.js
@@ -1,39 +1,39 @@
-const firstLine = '#  #  #  #';
-const secondLine = '#  #  #  #';
-const sizeOfBoard = 8;
+// const firstLine = '#  #  #  #';
+// const secondLine = '#  #  #  #';
+// const sizeOfBoard = 8;
 
-const chessBoard = function(size) {
- for (let i = 1; i <= 8; i++) {
-  if (i % 2 === 0) {
-   console.log(secondLine);
-  }
-  else {
-   console.log(firstLine);
-  }
- }
-}
-console.log(chessBoard());
-
-// VARIABLES
-// let col;
-// let row;
-
-// function chessBoard(size) {
-//  let spChar = '';  //declaring spChar an empty string
-//  // LOOPING OVER THE ROW
-//  for (let row = 1; row <= size; row++) {
-
-//   // IF ROW HAS A VALUE THAT IS EVEN
-//   if (row % 2 === 0) {
-//    spChar += ''; // empty string
+// const chessBoard = function(size) {
+//  for (let i = 1; i <= 8; i++) {
+//   if (i % 2 === 0) {
+//    console.log(secondLine);
 //   }
-//   // LOOPING OVER THE COLUMN
-//   for (let col = 1; col <= size; col++) {
-//    spChar += ' #';
+//   else {
+//    console.log(firstLine);
 //   }
-//   spChar += '\n';
 //  }
-//  return spChar;
 // }
+// console.log(chessBoard());
 
-// console.log(chessBoard(8));
+VARIABLES
+let col;
+let row;
+
+function chessBoard(size) {
+ let spChar = '';  //declaring spChar an empty string
+ // LOOPING OVER THE ROW
+ for (let row = 1; row <= size; row++) {
+
+  // IF ROW HAS A VALUE THAT IS EVEN
+  if (row % 2 === 0) {
+   spChar += ''; // empty string
+  }
+  // LOOPING OVER THE COLUMN
+  for (let col = 1; col <= size; col++) {
+   spChar += ' #';
+  }
+  spChar += '\n';
+ }
+ return spChar;
+}
+
+console.log(chessBoard(8));
